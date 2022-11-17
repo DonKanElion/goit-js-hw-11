eaadd6164d0d9853a5f71329cbd10400fec04037
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-11-17 23:50:41 +0200

    first commit

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+
+const URL_BASE = 'https://restcountries.com/v3.1';
+
+export function fetchCountries(country) {
+   return fetch(`${URL_BASE}/name/${country}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+        if(!response.ok || response.status === 404){
+            // throw new Error(Notiflix.Notify.failure('Oops, there is no country with that name'));
+            throw new Error('Oops, there is no country with that name');
+        }
+        return response.json();
+    });
+}
