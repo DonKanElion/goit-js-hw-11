2f10a89c5f7283a2bb2abaef4aa5a59bf039a4f2
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-11-21 00:43:48 +0200

    Task Done!
    - Ще стилі допрацювати і все.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,77 @@
 import './css/styles.css';
-import axios from 'axios';
-import debounce from 'lodash.debounce';
+import markup from './templates/markup.hbs';
 import Notiflix from 'notiflix';
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './js/fetchImages';
 
-
 const refs = {
-    formEl: document.querySelector(".search-form"),
-    inputEl: document.querySelector(".search-field"),
-    btnEl: document.querySelector(".search-btn"),
-    galleryEl: document.querySelector(".gallery"),
+    searchForm: document.querySelector(".search-form"),
+    inputData: document.querySelector(".search-field"),
+    searchBtn: document.querySelector(".search-btn"),
+    gallery: document.querySelector(".gallery"),
+    loadMoreBtn: document.querySelector(".load-more"),
 };
 
-const DEBOUNCE_DELAY = 300;
+let lightbox = new SimpleLightbox('.gallery a');
+let page = 1;
 
-// console.log(refs.formEL);
-// console.log(refs.inputEl);
-// console.log(refs.btnEl);
-// console.log(refs.galleryEl);
-
-refs.inputEl.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
+refs.searchForm.addEventListener("submit", onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.style.display = 'none';
 
 function onSearch(evt){
-    const searchImg = refs.inputEl.value.trim();
+    evt.preventDefault();
+    clearImages();
+    const searchValue = refs.inputData.value.trim();
 
-       console.log(searchImg);
-// if(!searchImg){
-//     refs
-// }
-    fetchImages(searchImg)
+    if(searchValue){
+        fetchImages(searchValue, page)
+        .then(data => {
+            if(data.hits.length === 0) {
+                Notiflix.Notify.warning(
+                    'Sorry, there are no images matching your search query. Please try again.'
+                );
+            } else {
+                renderMarkup(data.hits);
+                Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+                refs.loadMoreBtn.style.display = 'block';
+                lightbox.refresh();
+            }
+        })
+        .catch(function (error) {
+            console.log('Error', error.message);
+        });
+    }
+    if(searchValue === ''){
+        Notiflix.Notify.failure('Oops, please enter data in the search field');
+    }
 
+};
 
-}
+function onLoadMore(evt) {
+    page += 1;
+    const searchValue = refs.inputData.value.trim();
 
+    fetchImages(searchValue, page).then(data => {
+        renderMarkup(data.hits);
+        lightbox.refresh();
+        const totalPage = data.totalHits / 40;
+
+        if(totalPage <= page) {
+            refs,loadMoreBtn.style.display = 'none';
+            Notiflix.Notify.warning("We're sorry, but you've reached the end of search results."
+            )
+        }
+    })
+};
+
+function renderMarkup(images) {
+    refs.gallery.insertAdjacentHTML('beforeend', markup(images));
+  }
+
+function clearImages(){
+    page = 1;
+    refs.loadMoreBtn.style.display = 'none';
+    refs.gallery.innerHTML = '';
+};
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,14 +1,13 @@
+import axios from 'axios';
+
+const axios = require('axios').default;
 const URL_BASE = 'https://pixabay.com/api/';
 const API_KEY = '31409515-1e05b025820d8f08d6d70aee0';
 
-export function fetchImages(img) {
-    return fetch(`${URL_BASE}/?key=${API_KEY}&q=${img}&image_type=photo&orientation=horizontal&safesearch=true`)
-    .then(response => {
-        if(!response.ok || response.status === 404){
-            throw new Error('Oops, there is no images with that name');
-        }
-
-        console.log(response.json());
-        return response.json();
-    });
+export async function fetchImages(searchQuery, page) {
+  const response = await axios.get(
+    `${URL_BASE}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  );
+  console.log(response.data);
+  return response.data;
 }
