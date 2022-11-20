455856abe7ae8b57011532a017c2e42cdb5b7977
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-11-20 21:07:42 +0200

    add

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,10 @@
 const URL_BASE = 'https://pixabay.com/api/';
 const API_KEY = '31409515-1e05b025820d8f08d6d70aee0';
 
+const options ={
+    
+}
+
 export function fetchImages(img) {
     return fetch(`${URL_BASE}/?key=${API_KEY}&q=${img}&image_type=photo&orientation=horizontal&safesearch=true`)
     .then(response => {
